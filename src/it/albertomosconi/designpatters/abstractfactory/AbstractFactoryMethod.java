a9640331209d018176e8532fab96654fd5829d92
Alberto Mosconi <albertomaria.mosconi@gmail.com>
2020-09-13 20:02:21 +0200

    Setup for Abstract Factory Method

---
+++ src/it/albertomosconi/designpatters/abstractfactory/AbstractFactoryMethod.java
@@ -0,0 +1,13 @@
+package it.albertomosconi.designpatters.abstractfactory;
+
+import it.albertomosconi.designpatters.Pattern;
+
+public class AbstractFactoryMethod implements Pattern {
+    @Override
+    public void execute() {
+        System.out.println("ABSTRACT FACTORY PATTERN");
+
+
+        System.out.println();
+    }
+}
